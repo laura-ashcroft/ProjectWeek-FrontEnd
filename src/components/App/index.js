4518ffc39b0980ab60320dd9102c50a4a65cb21b
= <=>
2020-11-11 14:44:37 +0000

    cards displayed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,35 +4,40 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 //Homepages
 import HomePage from "../../Pages/Homepage";
-//Mentor Pages
+// Mentor Pages
 import MentorSignIn from "../SignInMentor/index";
 import MentorHomepage from "../../Pages/MentorsMain/index";
 import MentorProfile from "../../Pages/MentorProfile/index";
-//Bootcamper Pages
+// Bootcamper Pages
 import BootcamperSignIn from "../SignInBootcamper/index";
 import BootcamperHomepage from "../../Pages/BootcamperMain/index";
+import BootcamperMatch from "../../Pages/BootcamperMatch/index";
 //Coach Pages
 import Coaches from "../../Pages/CoachesMain";
 import CoachDisplayTables from "../../Pages/CoachDisplayTables";
 
 function App() {
   return (
-    <BootcamperHomepage />
-    // <Router>
-    //   <div className="App">
-    //     <Button>
-    //       <Link to="/Coaches">Coach</Link>
-    //     </Button>
-    //     <nav className="navButtons">
-    //       <HomePage />
-    //     </nav>
-    //     <Switch>
-    //       <Route path="text">
-    //         <Textpage />
-    //       </Route>
-    //     </Switch>
-    //   </div>
-    // </Router>
+    //<BootcamperMatch />
+    <>
+      <BootcamperMatch />
+      {/* <BootcamperHomepage />
+      <Router>
+        <div className="App">
+          <Button>
+            <Link to="/Coaches">Coach</Link>
+          </Button>
+          <nav className="navButtons">
+            <HomePage />
+          </nav>
+          <Switch>
+            <Route path="text">
+              <Textpage />
+            </Route>
+          </Switch>
+        </div>
+      </Router> */}
+    </>
   );
 }
 
--- src/components/MentorDisplayCards/index.js
+++ src/components/MentorDisplayCards/index.js
@@ -2,6 +2,56 @@
 import React from "react";
 
 //components
-
+import Checkbox from "../Checkbox/index";
 //css
 import styles from "./mentorDisplayCards.module.css";
+
+function MentorDisplayCards({ mentor, chosenFn, chosenArray }) {
+  return (
+    <div className={styles.card}>
+      <Checkbox
+        chosenFn={chosenFn}
+        name={mentor.name}
+        chosenArray={chosenArray}
+      />
+      <ul>
+        <li>{mentor.name}</li>
+        <li>
+          <strong>Present Job:</strong> {mentor.present_role}
+        </li>
+        <li>
+          <strong>Company:</strong>
+          {mentor.current_business}
+        </li>
+        <li>
+          <strong>Industry:</strong>
+          {mentor.industry}
+        </li>
+        <li>
+          <strong>Job Description:</strong> {mentor.role_description}
+        </li>
+        <li>
+          <strong>Speciality Language:</strong>
+          {mentor.speciality_language}
+        </li>
+        <li>
+          <strong>Coding Languages:</strong> {mentor.coding_languages}
+        </li>
+        <li>
+          <strong>Bio: </strong>
+          {mentor.bio}
+        </li>
+        <li>
+          <strong>Interests:</strong> {mentor.interests}
+        </li>
+        <li>
+          {mentor.previous_bootcamper
+            ? "Is a previous bootcamper"
+            : "Is not a previous bootcamper"}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default MentorDisplayCards;
