55b74e9d57f9e463d3361581ee265899ed501c1e
Laura Ashcroft <laura.ashcroft14@gmail.com>
2020-11-11 10:52:40 +0000

    completed styling and table heads

--- src/Pages/CoachDisplayTables/index.js
+++ src/Pages/CoachDisplayTables/index.js
@@ -3,12 +3,20 @@ import React from "react";
 
 //components
 
+//images
+import soc from "../../Images/soc.png";
+
 //css
 import styles from "./coachTables.module.css";
 
 function CoachDisplayTables() {
   return (
     <div className={styles.tables}>
+      <img className={styles.socLogo} src={soc} alt="school of code logo" />
+      <h2>Welcome, Coach!</h2>
+      <p>
+        Information for Bootcampers and Mentors are found in the tables below.
+      </p>
       <h2>Bootcamper Information</h2>
       <table>
         <thead>
